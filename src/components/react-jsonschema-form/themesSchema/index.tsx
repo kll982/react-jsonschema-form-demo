d2c861b9ab2bbb6b5cd7b527c4c4dd86b59a4521
kll <kll982982@qq.com>
2022-07-12 10:38:35 +0800

    js=>tsx

--- src/components/react-jsonschema-form/themesSchema/index.tsx
+++ src/components/react-jsonschema-form/themesSchema/index.tsx
@@ -1,29 +1,46 @@
 import React, { useRef } from "react";
 import { Button } from "antd";
-import { withTheme } from "@rjsf/core";
-import { Theme as AntDTheme } from "@rjsf/antd";
+// import { withTheme } from "@rjsf/core";
+import RjsfForm from "@rjsf/antd";
+// import { Theme as AntDTheme } from "@rjsf/antd";
 
 import { schema, uiSchema } from "./utils";
 import { ColorPicker } from "../widgets";
+import { JSONSchema7 } from "json-schema";
+import { UiSchema } from "@rjsf/core";
 import "../index.less";
 
-const RjsfForm = withTheme(AntDTheme);
+// const RjsfForm = withTheme(AntDTheme);
 
-const ThemesForm = () => {
+const RjsfFormComponent: React.FC<any> = RjsfForm as any;
+
+interface RjsfProps {
+  schema?: JSONSchema7;
+  uiSchema?: UiSchema;
+  widgets?: object;
+  fields?: object;
+  className?: string;
+  children?: HTMLElement;
+  formData?: object;
+  onSubmit?: (val: object) => void;
+  onError?: (val: Array<object>) => void;
+}
+
+const ThemesForm = (props: RjsfProps) => {
   // 覆盖原来的 组件
   const widgets = {
     ColorWidget: ColorPicker,
   };
-  const onSubmit = ({ formData }, e) =>
+  const onSubmit = ({ formData }: { formData: object }) =>
     console.log("Data submitted: ", formData);
 
-  const onError = (errors) => {
+  const onError = (errors: Array<object>) => {
     console.log("errors", errors);
   };
 
   return (
     <div className={"form"}>
-      <RjsfForm
+      <RjsfFormComponent
         schema={schema}
         uiSchema={uiSchema}
         widgets={widgets}
@@ -41,7 +58,7 @@ const ThemesForm = () => {
             保存当前配置
           </Button>
         </div>
-      </RjsfForm>
+      </RjsfFormComponent>
     </div>
   );
 };
---
+++ src/components/react-jsonschema-form/type.d.ts
@@ -0,0 +1,8 @@
+declare module "@rjsf/antd" {
+  type RJSfForm = {
+    schema: JSONSchema7;
+    uiSchema: UiSchema;
+  };
+}
+
+declare module "lodash-contrib" {}
